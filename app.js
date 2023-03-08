885122d1af6f4be40b25823b98e594343057765d
bilguun <bilguundorj@gmail.com>
2023-03-08 14:02:40 +0800

    dom ruu handah hicheel

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var player = 0 
+// Toglogchinn eeljiig uurchilj, 1r toglolgchiig 0 , 2r toglogchiig 1 gej temdegley
+var activePlayer = 1;
+
+//Toglogchdiin onoo hadgalah huvisagch 
+var scores=[0,0];
+//Toglogchiin eeljind avch baigaa onoog hadgalah huvisagch
+var playerScore = 0;
+//Shoonii ali talaaraa buuj bgaag hadgalah huvisagch heregtei 
+var dice = Math.random()*6;
+
+console.log(dice);
