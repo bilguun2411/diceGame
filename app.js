5e69ace2c68409ba07ac1ed8a912a277e6ffd68b
bilguun <bilguundorj@gmail.com>
2023-03-08 14:12:48 +0800

    Project iin ehni huvisagchdiig uusgesen

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ var scores=[0,0];
 //Toglogchiin eeljind avch baigaa onoog hadgalah huvisagch
 var playerScore = 0;
 //Shoonii ali talaaraa buuj bgaag hadgalah huvisagch heregtei 
-var dice = Math.random()*6;
+var dice = Math.floor(Math.random()*6+1);
+
+window.document.querySelector('#score-0').textContent = 0;
+window.document.querySelector('#current-0').textCongtent = 0
+document.querySelector('#score-1 ').textContent = 0;
+document.querySelector('#score-1 ').textContent = 0 
+ 
+document.querySelector('.dice').style.display = 'none';
 
 console.log(dice);
